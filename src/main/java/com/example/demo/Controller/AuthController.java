7c75497db3b4534fbde62c2f4921bd55094d843c
bala-amypo <bala@amypo.in>
2025-12-19 06:36:34 +0000

    User commit

---
+++ src/main/java/com/example/demo/Controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.RegisterRequest;
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody RegisterRequest request) {
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword()); // Will be hashed in service
+        user.setRole(request.getRole() != null ? request.getRole() : "USER");
+
+        User savedUser = userService.register(user);
+        return ResponseEntity.ok(savedUser);
+    }
+}
