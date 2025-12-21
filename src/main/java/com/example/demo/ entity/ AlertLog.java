4b6922fb801b9529914a2adf8a7541208f94db21
bala-amypo <bala@amypo.in>
2025-12-21 16:16:03 +0000

    User commit

---
+++ src/main/java/com/example/demo/ entity/ AlertLog.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class AlertLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Warranty warranty;
+
+    private LocalDateTime sentAt;
+    private String message;
+
+    @PrePersist
+    void onCreate() {
+        sentAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/example/demo/ entity/Warranty.java
@@ -0,0 +1,26 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Warranty {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Product product;
+
+    private LocalDate purchaseDate;
+    private LocalDate expiryDate;
+
+    @Column(unique = true)
+    private String serialNumber;
+}
